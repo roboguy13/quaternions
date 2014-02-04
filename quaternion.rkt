7ced5c4b94455a60ac13da7d921580d09062507c
David Young <roboguy06@gmail.com>
2014-02-03 19:42:24 -0600

    Separated things into multiple files

--- quaternion.rkt
+++ quaternion.rkt
@@ -1,39 +1,17 @@
-#lang web-server/insta
+#lang racket
 
-(require mzlib/string)
-(define-namespace-anchor a)
-(define env (namespace-anchor->namespace a))
+(require "matrix.rkt")
 
-(define vector-sum
-  (curry map +))
-
-(define matrix-sum
-  (curry map vector-sum))
-
-(define (transpose m)
-  (if (or (null? m)
-          (null? (car m)))
-      '()
-      (cons (map car m)
-            (transpose (map cdr m)))))
-
-
-(define (matrix-prod orig-a orig-b)
-  (define (go a b)
-    (if (or (null? a)
-            (null? b))
-        '()
-        (cons (map (curry foldr + 0)
-                   (map (curry map * (car a))
-                        b))
-              (go (cdr a)
-                  b))))
-  (go orig-a (transpose orig-b)))
-
-(define (matrix-scalar-prod a m)
-  (map (curry map (curry * a))
-       m))
-
+(provide quaternion-sum
+         quaternion-prod
+         quaternion-mag
+         quaternion-inv
+         quaternion-eq?
+         quaternion-diff
+         quaternion-div
+         quaternion-log
+         quaternion-exp
+         quaternion-expt)
 
 (define (quaternion->matrix q)
   (matrix-sum
@@ -179,139 +157,3 @@
 ; quaternion-sin   ;
 ; quaternion-cos   ;
 ;------------------;
-  
-;--If the string is a number, returns it as a number, otherwise as a symbol
-(define (string->symOrNum str)
-  (if (number? (string->number str))
-               (string->number str)
-               (string->symbol str)))
-
-;--This can handle L in both cases:
-;--  '(+ ## ##i ...) - not necessarily complete
-;--  '##+##i+... - not necessarily complete
-;--Note that there are no spaces between numbers and + symbols in the latter example
-(define (addition->quaternion L)
-  (cond
-    ((null? L) '(0 0 0 0))
-    ((regexp-match? #rx"[1-9i-k]\\+" (~a L)) (addition->quaternion (cons '+ (map string->symOrNum (regexp-split #rx"\\+" (~a L)))))) ;first example and needs to be split
-    ((number? L) (list L 0 0 0)) ;##
-    ((symbol? L) (if (equal? (string-length (~a L)) 1) ;a single i, j, or k
-                     (addition->quaternion (string->symbol (string-append "1" (~a L))))
-                     (case (substring (~a L) (- (string-length (~a L)) 1) (string-length (~a L))) ;##i, ##j, or ##k
-                       (("i") (list 0 (string->number (substring (~a L) 0 (- (string-length (~a L)) 1))) 0 0))
-                       (("j") (list 0 0 (string->number (substring (~a L) 0 (- (string-length (~a L)) 1))) 0))
-                       (("k") (list 0 0 0 (string->number (substring (~a L) 0 (- (string-length (~a L)) 1))))))))
-    ((eq? '+ (car L)) ;(+ ## ##i ...) or (+) from recursion
-     (if(null? (cdr  L)) ;just (+)
-        '(0 0 0 0)
-        (vector-sum (addition->quaternion (cadr L)) (addition->quaternion (cons '+ (cddr L)))))))) 
-
-
-;---------------------------------------------;
-; Here is the evaluation part of the program. ;
-; It's still a work in progress, clearly.     ;
-;---------------------------------------------;
-
-;--Takes input in Scheme Standard notation
-;--As it is, this returns a quaternion
-(define (quaternion-eval E)
-  (cond
-    ((null? E) '())
-    ((number? E) (addition->quaternion E))
-    ((symbol? E) (addition->quaternion E)) ;in the form of #+#i+...
-    (else
-     (case (car E)
-       ((+) (cond
-              ((equal? (length E) 1) 0) ;no arguments - 0 per Scheme Standard
-              ((equal? (length (cdr E)) 1) (quaternion-eval (cadr E))) ;one argument - equals itself
-              (else (quaternion-sum (quaternion-eval (cadr E)) (quaternion-eval (cons '+ (cddr E))) ))))
-       ((-) (if (equal? (length (cdr E)) 1) ;just one argument, so negative, not subtraction
-                (quaternion-eval (list '* -1 (cadr E)))
-                (quaternion-diff (quaternion-eval (cadr E)) (map quaternion-eval (cddr E)))))
-       ((*) (cond
-              ((equal? (length E) 1) 1) ;no arguments - 1 per Scheme Standard
-              ((equal? (length (cdr E)) 1) (quaternion-eval (cadr E))) ;one argument - equals itself
-              (else (quaternion-prod (quaternion-eval (cadr E)) (quaternion-eval (cons '* (cddr E))) ))))
-       ((/) (if (equal? (length (cdr E)) 1) ;one argument - inverse, not division
-                (quaternion-eval (list '/ 1 (cadr E)))
-                (quaternion-div (quaternion-eval (cadr E)) (map quaternion-eval (cddr E)))))
-       ((exp) (quaternion-exp (quaternion-eval (cadr E))))
-       ((expt) (quaternion-expt (quaternion-eval (cadr E)) (quaternion-eval (caddr E))))
-       ((log) (quaternion-log (quaternion-eval (cadr E))))
-       ((sin) '() )
-       ((cos) '() )
-       ((magnitude) (quaternion-mag (quaternion-eval (cadr E))))
-     ))))
-
-
-
-
-
-;----------------------------------------------------;
-; This is a direct copy-paste from my final project, ;
-; with a few modifications. Work in progress.        ;
-;----------------------------------------------------;
-
-(struct blog (posts) #:mutable)
-(struct post (expression))
-(define BLOG
-  (blog '()))
-
-(define (start request) (render-blog-page request))
-
-(define (parse-post bindings)
-  (post (extract-binding/single 'expression bindings)))
-
-(define (render-blog-page request)
-  (local [(define (response-generator make-url)
-            (response/xexpr
-             `(html (head (title "Quaternion Evaluation"))
-                    (body
-                     (h1 "Quaternion Evaluation") 
-                     (h2 "A project by Team Fourth Dimension")
-                     (form((action
-                            ,(make-url insert-post-handler)))
-                          "Expression to Evaluate:"
-                          (br)
-                          (input ((name "expression") (value "(* 2i+k 4+j+8k)")))
-                          (input ((type "submit"))))
-                     (a ((href "http://mathworld.wolfram.com/Quaternion.html"))
-                        "What is a quaternion?")
-                     (div ((id "history")) ,(render-posts))
-                     (br)))))
-          (define (insert-post-handler request)
-            (blog-insert-post!
-             BLOG (parse-post (request-bindings request)))
-            (render-blog-page request))]
-    (send/suspend/dispatch response-generator)))
-
-(define (blog-insert-post! a-blog a-post)
-  (set-blog-posts! a-blog (cons a-post (blog-posts a-blog))))
-
-
-(define (render-post a-post)
-  (define expr (if (equal? (string-length (post-expression a-post)) 0)
-                   '()
-                   (read-from-string(post-expression a-post))))
-      `(div ((class "post"))
-            (hr)
-            (p,"Input: ", (expr->string expr))
-            (p,"Output: ",(expr->string(quaternion-eval expr)))))
-
-
-;--Put CSS here
-(define (render-posts)
-  `(div ((class "posts"))
-        '(style ((type "text/css"))
-                "body { text-align:center; background-image:url('http://wallpoper.com/images/00/22/77/94/pattern-other_00227794.jpg') }"
-                "form { font-family: Verdana; color:#ddd; text-align:center }"
-                "input { font-family: Verdana; margin:3px }"
-                "h1 { font-family: Helvetica; color:#ddd; padding-top:20px }"
-                "h2 { font-family: Helvetica; color:#c00; margin-top:-20px; font-size:16px }"
-                "h3 { font-family: Helvetica; color:#ddd; font-size:20px }"
-                "#history { width:940px; overflow:hidden; margin: 10px auto; padding 0 0 20px 20px; background:#333; border-radius: 15px 15px 15px 15px}"
-                "p { font-family: Verdana; color:#ddd }"
-                "a:link {color:#aaa}"
-                "a:visited {color:#777}")
-        (h3 "History:")
-        ,@(map render-post (blog-posts BLOG))))
